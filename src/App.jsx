d0818d92c59e3a6884fd345b584f394601df6dbd
prsprshr@gmail.com <123573626+ParasParashar@users.noreply.github.com>
2024-02-07 13:04:15 +0530

    update the product page

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,13 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
-
+import ProductPage from "./Pages/ProductPage";
+import { Routes, Route } from "react-router-dom";
 function App() {
   return (
-<<<<<<< HEAD
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/products" element={<ProductPage />} />
     </Routes>
-=======
-    <div className="flex flex-col min-h-screen bg-gradient-to-bl from-[#ede5de] to-[#6c778f] custom-scrollbar">
-      <Header />
-      <Hero />
-      <Footer />
-    </div>
->>>>>>> 1f6dd2d5c1f06d730da3a4a03198a716658e2f55
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,14 +7,6 @@ const Header = () => {
     <header className="flex flex-row py-2  px-5 items-center justify-between md:px-10 w-full   bg-[#ffffff]   to-[#ffffff] ">
       <Logo />
       <div className="flex gap-x-4 font-serif text-md    transition-all duration-200 md:text-lg">
-        <a
-          className="hover:text-gray-600"
-          href="https://www.dunagiri.com/about-us"
-          target="_blank"
-        >
-          About us
-        </a>
-
         <Link to={"/"}>Home</Link>
         <Link to="/products">products</Link>
       </div>
