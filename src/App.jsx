4e5e4c6bdb736214f877fd9e431491595d017db8
GOUTAMBHATI66 <goutambhati183@gmail.com>
2024-02-07 13:04:42 +0530

    updated me

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,14 @@ import HomePage from "./Pages/HomePage";
 
 function App() {
   return (
-<<<<<<< HEAD
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <div className="flex flex-col min-h-screen bg-gradient-to-bl from-[#ede5de] to-[#6c778f] custom-scrollbar">
+        <Header />
+        <Hero />
+        <Footer />
+      </div>
     </Routes>
-=======
-    <div className="flex flex-col min-h-screen bg-gradient-to-bl from-[#ede5de] to-[#6c778f] custom-scrollbar">
-      <Header />
-      <Hero />
-      <Footer />
-    </div>
->>>>>>> 1f6dd2d5c1f06d730da3a4a03198a716658e2f55
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-<<<<<<< HEAD
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
-=======
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
->>>>>>> 1f6dd2d5c1f06d730da3a4a03198a716658e2f55
 );
